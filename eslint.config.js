c7ce5c2709befdd468cdd18e74930bdee93a0de1
mtrasobaresb <miguel.trasoabres.baselga@gmail.com>
2026-07-06 19:56:46 +0200

    Modified ESLint and Prettier for conforming with modern ES Modules used in the project.

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,24 @@
-let customConfig = [];
-let hasIgnoresFile = false;
-try {
-  require.resolve('./eslint.ignores.js');
-  hasIgnoresFile = true;
-} catch {
-  // eslint.ignores.js doesn't exist
-}
+import gts from 'gts';
 
-if (hasIgnoresFile) {
-  const ignores = require('./eslint.ignores.js');
-  customConfig = [{ignores}];
-}
+export default [
+  // Spread the standard Google TypeScript Style rules
+  ...gts,
 
-module.exports = [...customConfig, ...require('gts')];
+  // Global ignore pathways (Replaces eslint.ignores.js)
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'vite.config.ts',
+      'eslint.config.js'
+    ]
+  },
+
+  // Custom project rule overrides
+  {
+    files: ['**/*.ts'],
+    rules: {
+      // If you want to customize any Google rules later, they go here
+    }
+  }
+];
